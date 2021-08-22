5a821dcc4f02fedff95969890b257bed035d2b85
sequws <workspaceshare@gmail.com>
2021-08-22 22:31:48 +0200

    Add project files.

---
+++ SimpleQuizCreator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace SimpleQuizCreator.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "SimpleQuizCreator v0.01b";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
