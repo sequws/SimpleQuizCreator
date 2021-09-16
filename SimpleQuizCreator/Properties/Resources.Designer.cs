b3ca384235fe76fbbcea9916bcb9a3c2660e1809
sequws <workspaceshare@gmail.com>
2021-09-17 00:20:17 +0200

    Add main icon

--- SimpleQuizCreator/Properties/Resources.Designer.cs
+++ SimpleQuizCreator/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace SimpleQuizCreator.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap QuizIcon {
+            get {
+                object obj = ResourceManager.GetObject("QuizIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
