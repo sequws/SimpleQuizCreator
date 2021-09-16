8426f8fe5fc4e77c4c98c778d6980d88aea8cb26
sequws <workspaceshare@gmail.com>
2021-09-17 01:07:00 +0200

    Add setting class.

---
+++ SimpleQuizCreator/Models/QuizSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuizCreator.Models
+{
+    public enum QuizType
+    {
+        SingleChoice,
+        MultiChoice
+    }
+
+    public enum ScoreType
+    {
+        OneGoodZeroBad,         //  1pts for good answer, no minus points for bad answer
+        OneGoodOneBad,          //  1pts for good answer, -1pts points for bad answer
+        AllGoodWithoutMinus,    //  1pts for all good answers checked, zero otherwise
+        AllGoodWithMinus,       //  1pts for all good answers checked, -1pts otherwise
+        Custom                  //  
+    }
+
+    public class QuizSettings
+    {
+        public bool TimeRescriction { get; set; } = false;
+        public bool AllowReturn { get; set; } = false;
+        // Scoring
+        public QuizType Type { get; set; } = QuizType.SingleChoice;
+        public ScoreType ScoreType { get; set; } = ScoreType.OneGoodZeroBad;
+
+        // Visibility
+        public bool ShowTime { get; set; } = false;
+        public bool ShowScore { get; set; } = false;
+
+        // Questions
+        public byte QuestionLimit { get; set; } = 30;
+        public byte AutogenerateAnswers { get; set; } = 4;
+    }
+}
