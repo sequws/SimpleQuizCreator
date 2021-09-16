2eac9bde46caee4a9ff9fc1a3d07a863163ffbc4
sequws <workspaceshare@gmail.com>
2021-09-16 23:50:30 +0200

    Update StartQuizView

--- SimpleQuizCreator/ViewModels/StartQuizViewModel.cs
+++ SimpleQuizCreator/ViewModels/StartQuizViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using SimpleQuizCreator.Interfaces;
+using SimpleQuizCreator.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,25 @@ namespace SimpleQuizCreator.ViewModels
     public class StartQuizViewModel : BindableBase
     {
         IWindowView _windowView;
+        IQuizService _quizService;
+        public List<Quiz> ListOfQuizzes { get; set; }
+
+        private Quiz _selectedQuiz = null;
+        public Quiz SelectedQuiz
+        {
+            get { return _selectedQuiz; }
+            set { SetProperty(ref _selectedQuiz, value); }
+        }
 
         private DelegateCommand _openQuizWindow;
         public DelegateCommand OpenQuizWindow =>
             _openQuizWindow ?? (_openQuizWindow = new DelegateCommand(ExecuteOpenQuizWindow, CanExecuteOpenQuizWindow));
 
-        public StartQuizViewModel(IWindowView windowView)
+        public StartQuizViewModel(IWindowView windowView, IQuizService quizService)
         {
             _windowView = windowView;
+            _quizService = quizService;
+            ListOfQuizzes = new List<Quiz>(_quizService.GetAllQuizzes());
         }
 
         void ExecuteOpenQuizWindow()
