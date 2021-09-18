95f9417c8f5a0ce941be7abd73e55fa4a8695fc6
sequws <workspaceshare@gmail.com>
2021-09-19 01:59:36 +0200

    Add QuizGenerated class

---
+++ SimpleQuizCreator/Models/QuizGenerated.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuizCreator.Models
+{
+
+    public class QuizGenerated
+    {
+        public List<Question> Questions = new List<Question>();
+        public Question ActiveQuestion { get; set; }
+        public int ActiveQuestionNumber { get; set; } = 0;
+
+        public bool TestFinished { get; private set; } = false;
+        public bool TestStarted { get; private set; } = false;
+
+        private QuizSettings quizSettings;
+    }
+}
