68e186ef62c378404baed15339dd7a6d703ae7f9
sequws <workspaceshare@gmail.com>
2021-09-29 23:00:51 +0200

    Add ScoreTypeToStringConverter for displaying custom name for score type

---
+++ SimpleQuizCreator/Converters/ScoreTypeToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SimpleQuizCreator.Converters
+{
+    public class ScoreTypeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strEnum;
+
+            try
+            {
+                strEnum = Enum.GetName((value.GetType()), value);
+                return strEnum;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
