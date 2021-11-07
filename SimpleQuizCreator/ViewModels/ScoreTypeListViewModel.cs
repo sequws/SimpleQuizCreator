732dd511c3fd9637d1079e168c6f309a83f01d5d
sequws <workspaceshare@gmail.com>
2021-11-07 12:46:19 +0100

    Add localization for ScoreType combobox

--- SimpleQuizCreator/ViewModels/ScoreTypeListViewModel.cs
+++ SimpleQuizCreator/ViewModels/ScoreTypeListViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Prism.Mvvm;
 using SimpleQuizCreator.Common;
 using SimpleQuizCreator.Models;
+using SimpleQuizCreator.Resources;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +15,7 @@ namespace SimpleQuizCreator.ViewModels
     public class ScoreTypeListViewModel
     {
         private List<ScoreTypeComboItem> scoreTypes = new List<ScoreTypeComboItem>();
+        ResourceManager rm = new ResourceManager(typeof(EnumResources));
 
         public ScoreTypeListViewModel()
         {
@@ -20,31 +23,36 @@ namespace SimpleQuizCreator.ViewModels
             {
                 Type = ScoreType.OneGoodZeroBad,
                 IsSingleAnswer = true,
-                Desc = "+1 for any good answer, 0 otherwise"
+                Desc = rm.GetString("OneGoodZeroBad")
+                //Desc = "+1 for any good answer, 0 otherwise"
             });
             scoreTypes.Add(new ScoreTypeComboItem
             {
                 Type = ScoreType.OneGoodOneBad,
                 IsSingleAnswer = true,
-                Desc = "+1 for any good answer, -1 for any bad answer, 0 otherwise"
+                Desc = rm.GetString("OneGoodOneBad")
+                //Desc = "+1 for any good answer, -1 for any bad answer, 0 otherwise"
             });
             scoreTypes.Add(new ScoreTypeComboItem
             {
                 Type = ScoreType.OneGoodOneBadOneNo,
                 IsSingleAnswer = true,
-                Desc = "+1 for any good answer, -1 otherwise"
+                Desc = rm.GetString("OneGoodOneBadOneNo")
+                //Desc = "+1 for any good answer, -1 otherwise"
             });
             scoreTypes.Add(new ScoreTypeComboItem
             {
                 Type = ScoreType.AllGoodWithoutMinus,
                 IsSingleAnswer = false,
-                Desc = "+1 for all good answers, 0 otherwise"
+                Desc = rm.GetString("AllGoodWithoutMinus")
+                //Desc = "+1 for all good answers, 0 otherwise"
             });
             scoreTypes.Add(new ScoreTypeComboItem
             {
                 Type = ScoreType.AllGoodWithMinus,
                 IsSingleAnswer = false,
-                Desc = "+1 for all good answers, -1 otherwise"
+                Desc = rm.GetString("AllGoodWithMinus")
+                //Desc = "+1 for all good answers, -1 otherwise"
             });
         }
 
