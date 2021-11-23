2ca152e18109f62298ec50f42f6d9cfb0d15a50f
sequws <workspaceshare@gmail.com>
2021-11-23 22:42:52 +0100

    Add int to time converter in HistoryView

---
+++ SimpleQuizCreator/Converters/IntToStringTimeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SimpleQuizCreator.Converters
+{
+    public class IntToStringTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if( !string.IsNullOrEmpty(value.ToString()))
+            {
+                TimeSpan time = TimeSpan.FromSeconds(int.Parse(value.ToString()));
+                return time.ToString(@"hh\:mm\:ss");
+            }
+            return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
